b7649f5f54fd2d81c6a00d3f39c7624bca610aa2
robertriosm <87707371+robertriosm@users.noreply.github.com>
2023-02-10 20:41:09 -0600

    avances

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 
 class BayesianNetwork:
+
     def __init__(self, nodes) -> None:
         self.graph = {}
         for i,j in nodes.items():
@@ -12,30 +13,49 @@ class BayesianNetwork:
     def __repr__(self) -> str:
         return ''.join([(f"{i}: {j}\n") for i,j in self.graph.items()])
     
-    def setNodeP(self, nodeName, probability):
-            if nodeName in self.graph.keys():
-                f = 0
-                for i in self.graph:
-                    if nodeName in self.graph[i]:
-                        f = 1
-                if f == 0:
-                    self.tables[nodeName] = probability
-                else:
-                    print("node not independent")
 
+    def setParentNodeProb(self, nodeName, probability:int):
+        if nodeName in self.graph.keys():
+            f = 0
+            for i in self.graph:
+                if nodeName in self.graph[i]:
+                    f = 1
+            if f == 0:
+                self.tables[nodeName] = probability
             else:
-                print("node not in graph")
+                print("node not independent")
+        else:
+            print("node not in graph")
+
+
+    def setDepNodeProb(self, nodeName):
+
+        if nodeName in self.graph.keys():
+
+            self.tables[nodeName] += 0
+
+
+
+
+
+
+
+            for child in self.graph.values():
+                pass
+
+
 
 
 bn = BayesianNetwork({"Burglary": ["Alarm"], "Earthquake":["Alarm"], "Alarm": ["JohnCalls", "MaryCalls"]})
 
 print(bn)
 
-bn.setNodeP("Burglary",0.9)
-bn.setNodeP("Earthquake",0.2)
-bn.setNodeP("Alarm",0.5555)
-bn.setNodeP("Alarm",0.5555)
-bn.setNodeP("Alarm",0.5555)
-bn.setNodeP("Alarm",0.5555)
-bn.setNodeP("Ye",0)
+bn.setParentNodeProb("Burglary", 0.9)
+bn.setParentNodeProb("Earthquake", 0.2)
+bn.setParentNodeProb("Alarm", 0.5555)
+bn.setParentNodeProb("Alarm", 0.5555)
+bn.setParentNodeProb("Alarm", 0.5555)
+bn.setParentNodeProb("Alarm", 0.5555)
+bn.setParentNodeProb("Ye",0)
 print(bn.tables)
+print(bn.graph)
